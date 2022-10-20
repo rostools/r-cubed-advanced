d4c55fe54a9aadc8a07bdd110a97d62a561f4af3
Luke W. Johnston <lwjohnst@gmail.com>
2022-10-19 23:26:35 -0400

    add more comments to data raw script

--- data-raw/nmr-omics.R
+++ data-raw/nmr-omics.R
@@ -1,20 +1,28 @@
-## code to prepare `nmr-omics` dataset goes here
+# Load necessary packages -------------------------------------------------
 
 library(readxl)
 library(dplyr)
 library(tidyr)
 library(here)
 
+# Download dataset --------------------------------------------------------
+
 # From DOI: 10.5281/zenodo.6597902
+# Direct URL: https://zenodo.org/record/6597902
+
 # Get both README and the Lipidomics dataset.
 nmr_omics_dir <- here("data-raw/nmr-omics")
 fs::dir_create(nmr_omics_dir)
 
 download.file("https://zenodo.org/record/6597902/files/README.txt",
-              destfile = here(nmr_omics_dir, "README.txt"))
+    destfile = here(nmr_omics_dir, "README.txt")
+)
 
 download.file("https://zenodo.org/record/6597902/files/NMR_Integration_Data_Lipidomics.xlsx",
-              destfile = here(nmr_omics_dir, "lipidomics.xlsx"))
+    destfile = here(nmr_omics_dir, "lipidomics.xlsx")
+)
+
+# Wrangle dataset into tidy long format -----------------------------------
 
 lipidomics_full <- read_xlsx(
     here(nmr_omics_dir, "lipidomics.xlsx"),
@@ -28,25 +36,33 @@ lipidomics_full <- read_xlsx(
 # Keep only lipidomic values
 lipidomics_only <- lipidomics_full %>%
     # Want to remove columns 2, 3, and 4 since they are "limits"
+    # (we don't need them for this course)
     select(-2:-4) %>%
-    # Remove the subject data
+    # Remove the subject data rows
     slice(-1:-4) %>%
+    # Make it so the metabolite values are all in one column,
+    # which will make it easier to join with the subject data later.
     pivot_longer(-V1) %>%
-    rename(Metabolite = V1)
+    rename(metabolite = V1)
 
 # Keep only subject data
 subject_only <- lipidomics_full %>%
-    # Want to remove the first metabolic columns
+    # Remove the first metabolic name and limit columns,
+    # don't need for this
     select(-1:-3) %>%
-    # Keep only the subject data
+    # Keep only the subject data raw
     slice(1:4) %>%
     pivot_longer(cols = -V4) %>%
     pivot_wider(names_from = V4, values_from = value) %>%
     rename_with(snakecase::to_snake_case)
 
-lipidomics <- full_join(subject_only,
-                        lipidomics_only) %>%
+lipidomics <- full_join(
+    subject_only,
+    lipidomics_only
+) %>%
     # Don't need anymore
     select(-name)
+
+# Save to `data/` ---------------------------------------------------------
 
 usethis::use_data(lipidomics, overwrite = TRUE)
