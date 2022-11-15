240bf550ab0870e908f48dd46f308fca81d2ac5a
Luke W. Johnston <lwjohnst@gmail.com>
2022-11-15 12:54:13 +0100

    helper functions for building project

--- R/build-project-functions.R
+++ R/build-project-functions.R
@@ -6,3 +6,25 @@ git_ci <- function(files, message) {
 write_to_file <- function(text, file, append = FALSE) {
   readr::write_lines(text, here::here(file), append = append)
 }
+
+revise_by_text <- function(path, original, replacement) {
+  readr::read_lines(here::here(path)) |>
+    stringr::str_replace(original, replacement) |>
+    readr::write_lines(here::here(path))
+}
+
+revise_by_line_num <- function(path, insert_text, remove_original_lines, insert_at_line) {
+  old_text <- readr::read_lines(here::here(path))
+
+  if (!missing(remove_original_lines)) {
+    old_text <- old_text[remove_original_lines]
+  }
+
+  if (!missing(insert_at_line)) {
+    insert_at_line <- length(old_text)
+  }
+
+  old_text |>
+    append(insert_text, after = insert_at_line) |>
+    readr::write_lines(here::here(path))
+}
